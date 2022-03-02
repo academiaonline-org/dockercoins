eaca294535252246365fa90cece9be268d5a1121
RafaMPA <100578443+RafaMPA@users.noreply.github.com>
2022-03-02 16:23:39 +0100

    Update docker-stop.sh

--- docker-stop.sh
+++ docker-stop.sh
@@ -1,11 +1,13 @@
+
+
+docker stop worker
 docker network disconnect redis worker
 docker network disconnect rng worker
 
-docker stop worker
 docker stop webui
 docker stop rng
 docker stop hasher
 
-docker network rm hasher
-docker network rm redis
-docker network rm rng
+#docker network rm hasher
+#docker network rm redis
+#docker network rm rng
